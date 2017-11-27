d772adce5604c16c55e6f8e94f9237196a9f1141
Apirat Chiwakanit <poom_popping@hotmail.com>
2017-11-27 09:44:17 +0700

    [3-16] การใช้งาน cookie-session

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,4 +1,15 @@
 exports.login = function(req, res) {
+	req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+	req.sanitizeBody('email').normalizeEmail();
+	var errors = req.validationErrors();
+	if( errors ) {
+		res.render('index', {
+			title: 'There have been validation errors' + JSON.stringify(errors),
+			isLoggedIn: false
+		});
+		return;
+	}
+
 	console.log(req.body);
 	console.log('Email: ' + req.body.email);
 	console.log('Password ' + req.body.password);
